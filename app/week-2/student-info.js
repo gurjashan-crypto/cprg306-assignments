b579c0b387288b62d901462bc64fca459be459d3
gurjashan-crypto <gurjashansingh298@gmail.com>
2025-01-27 10:30:47 -0700

    updated code for week 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+export default function StudentInfo() {
+  return (
+    <div>
+      <p> Gurjashanpreet Singh</p>
+      <a href="https://github.com/gurjashan-crypto?tab=repositories">View My GitHub Repository</a>
+    </div>
+  );
+}
+
+  
