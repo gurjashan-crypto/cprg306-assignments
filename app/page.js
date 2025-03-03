e29b8ec0c01f3715166dd5459c38785cf93fb6c4
gurjashan-crypto <gurjashansingh298@gmail.com>
2025-03-03 09:29:55 -0700

    week-6

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ export default function Page(){
     <p>Link to week 2: <Link href="week-2">Week 2</Link> </p>
     <p>Link to week 3: <Link href="week-3">Week 3</Link> </p>
     <p>Link to week 4: <Link href="week-4">Week 4</Link> </p>
-    <p>Link to week 5: <Link href="week-5">Week 5</Link> </p></div>
+    <p>Link to week 5: <Link href="week-5">Week 5</Link> </p>
+    <p>Link to week 6: <Link href="week-6">Week 6</Link> </p></div>
   );
 };
